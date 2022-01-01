be0407027f120a3865b8dd20b0be4f4320959345
pyhos <95932748+pyhos@users.noreply.github.com>
2022-01-01 15:33:58 +0300

    Add files via upload

---
+++ taşkağıtmakas.py
@@ -0,0 +1,34 @@
+import random
+
+secenek = ["taş", "kağıt", "makas"]
+taş = secenek[0]
+kağıt = secenek[1]
+makas = secenek[2]
+print("Taş kağıt makas oyununa hoşgeldin. Oyundan çıkmak için (q) tuşunu giriniz.")
+while True:
+    secim = input("Taş mı? Kağıt mı? Makas mı? ")
+    random.shuffle(secenek)
+    bil_secim = random.choice(secenek)
+    if(secim == taş):
+        if bil_secim == taş:
+            print("Bilgisayarın seçimi: Taş Sonuç: Berabere")
+        elif bil_secim == kağıt:
+            print("Bilgisayarın seçimi: Kağıt Sonuç: Kaybettiniz")
+        else:
+            print("Bilgisayarın seçimi: Makas Sonuç: Kazandınız")
+    if(secim == kağıt):
+        if bil_secim == taş:
+            print("Bilgisayarın seçimi: Taş Sonuç: Kazandınız")
+        elif bil_secim == kağıt:
+            print("Bilgisayarın seçimi: Kağıt Sonuç: Berabere")
+        else:
+            print("Bilgisayarın seçimi: Makas Sonuç:Kaybettiniz")
+    if(secim == makas):
+        if bil_secim == taş:
+            print("Bilgisayarın seçimi: Taş Sonuç: Kaybettiniz")
+        elif bil_secim == kağıt:
+            print("Bilgisayarın seçimi: Kağıt Sonuç: Kazandınız")
+        else:
+            print("Bilgisayarın seçimi: Makas Sonuç:Berabere")
+    if(secim == "q"):
+        break
